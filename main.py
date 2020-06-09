313ef7169ceccf0ec7a10b7141413764d797d587
Lydia Fox <66090474+lydia-fox@users.noreply.github.com>
2020-06-09 18:50:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
+TOKEN = open("discord.token","r").read()
 import discord
-
+import time
 BLOCK_TIME = 60 # secconds
 
 # network param vars
@@ -15,7 +15,7 @@ lBlockAlgo = None
 LBAD = 1
 AvgDiff = 1
 POWreward = 50
-LBTXNC = 1
+LBTxnC = 1
 MempoolTxnC = 0
 NetHash = "1 H/s"
 coins = 0
@@ -33,8 +33,9 @@ update_interval = 5 # secs
 node_addr = "127.0.0.1"
 node_port = "1234"
 node = None
+print("Starting")
 
-def update_stats(node):
+def update_stats():
   return True # TODO
   
 def connect_node():
@@ -52,23 +53,20 @@ def gen_embed():
   embed.add_field(name="Last Block Mined Algo", value=lBlockAlgo, inline=True)
   embed.add_field(name="Last Mined Algo Diff", value=LBAD, inline=True)
   embed.add_field(name="Avg. diff", value=AvgDiff, inline=True)
-  embed.add_field(name="Last PoW reward", value=POWreward + " COOF", inline=True)
+  embed.add_field(name="Last PoW reward", value=str(POWreward) + " COOF", inline=True)
   embed.add_field(name="Txn in last Block", value=LBTxnC, inline=True)
   embed.add_field(name="Txn in mempool", value=MempoolTxnC, inline=True)
   embed.add_field(name="Est. total hash rate", value=NetHash, inline=True)
-  embed.add_field(name="Coins in circulation", value=coins + " COOF", inline=True)
+  embed.add_field(name="Coins in circulation", value=str(coins) + " COOF", inline=True)
   embed.add_field(name="Active Wallet Count", value=active_wallets, inline=True)
   embed.add_field(name="Next Epoch Height", value=NextEpochHeight, inline=True)
   embed.add_field(name="Est Time to Next Epoch", value=TimeToEpoch, inline=True)
-  embed.add_field(name="Epoch Block", value=EBlock + "/10", inline=True)
+  embed.add_field(name="Epoch Block", value=str(EBlock) + "/10", inline=True)
   return embed
   
 async def on_ready_stats():
-    for server in client.servers:
-      if server.name == "Coinfection":
-        for channel in server.channels:
-          if channel.name == "blockchain-status":
-            update_channel = channel
+    global node, msg
+    update_channel = discord.utils.get(client.get_all_channels(), guild__name='Coinfection', name='blockchain-status')
     if update_channel == None:
       print("Channel not found... ")
       node = connect_node()
@@ -76,20 +74,31 @@ async def on_ready_stats():
       while True:
         if msg == None:
           # create a new one
-          update_stats(node) # update the stats vars
+          update_stats() # update the stats vars
           message = gen_embed() # get the stuff to send
-          await update_channel.send(embed=message)
+          msg = await update_channel.send(embed=message)
+
         else:
           # edit the old one
-          update_stats(node) # update the stats vars
+          update_stats() # update the stats vars
           message = gen_embed() # get the stuff to send
           await msg.edit(embed=message)
         time.sleep(update_interval)
 
-client = discord.Client()
+class CoofBot(discord.Client):
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+        await on_ready_stats()
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('!rain'):
+            await message.channel.send('{0.author.mention} will recieve 0 COOF'.format(message))
+
+client = CoofBot()
 client.run(TOKEN)
-print('Logged in as')
-print(bot.user.name)
-print(bot.user.id)
-print('------')
-on_ready_stats()
